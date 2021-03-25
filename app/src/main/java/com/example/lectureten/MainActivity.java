09f0125ae5be062386983c586eac8b79307bec2b
Kazak Kirill <kazzak.kirill1995@gmail.com>
2021-03-26 02:55:58 +0300

    First commit

---
+++ app/src/main/java/com/example/lectureten/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.lectureten;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
